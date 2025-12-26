53825d9f4a392d9fa9ac34760c24f9b03e9cad5e
alejandrallh <alejandrax.ll55@gmail.com>
2025-12-26 16:27:53 -0500

    se agrega ruta en boton verde

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,7 +1,7 @@
 // src/components/Layout/AppLayout.js
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Header"; // toma el index.js de Header
+import Header from "../Header"; 
 
 export default function AppLayout() {
   return (
